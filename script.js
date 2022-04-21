fe0fb98334344a4143963e5143a22e0d8a0e0c83
Denis <denisv.daros@gmail.com>
2022-04-20 23:02:41 -0300

    adicionando segundo requisito

---
+++ script.js
@@ -0,0 +1,4 @@
+const preto = document.getElementsByClassName("color")[0].style.backgroundColor = "black";
+const blue = document.getElementsByClassName("color")[1].style.backgroundColor = "blue";
+const amarelo = document.getElementsByClassName("color")[2].style.backgroundColor = "yellow";
+const vermelho = document.getElementsByClassName("color")[3].style.backgroundColor = "red";
