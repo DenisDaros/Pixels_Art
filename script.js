2ac81a06f5ad1baf0774b4f9c775bf0e5ad99ef4
Denis <denisv.daros@gmail.com>
2022-04-23 19:51:41 -0300

    adicionando 6 e 7 requisito

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const blue = document.getElementsByClassName("color")[1].style.backgroundColor =
 const amarelo = document.getElementsByClassName("color")[2].style.backgroundColor = "yellow";
 const vermelho = document.getElementsByClassName("color")[3].style.backgroundColor = "red";
 
+
+
+window.onload = function () {
+    let color = document.querySelectorAll(".color");
+   
+    for(index = 0; index < color.length;index += 1) {
+        color[index].addEventListener('click',function(e){
+            let seletor = document.querySelector(".selected")
+            seletor.classList.remove("selected")
+            e.target.classList.add("selected")
+        });
+    }
+    
+}
