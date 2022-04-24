50a7abb8a1d711ae40b154711845d85257616cd8
Denis <denisv.daros@gmail.com>
2022-04-24 00:36:00 -0300

    adicionando requisito 9

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 window.onload = function () {
     let color = document.querySelectorAll(".color");
-   
-    for(index = 0; index < color.length;index += 1) {
-        color[index].addEventListener('click',function(e){
+
+    for (index = 0; index < color.length; index += 1) {
+        color[index].addEventListener('click', function (e) {
             let seletor = document.querySelector(".selected")
             seletor.classList.remove("selected")
             e.target.classList.add("selected")
         });
     }
-    // https://stackoverflow.com/questions/9012537/how-to-get-the-element-clicked-for-the-whole-document
+    // link de referência https://stackoverflow.com/questions/9012537/how-to-get-the-element-clicked-for-the-whole-document
 
 
-let pixel = document.querySelectorAll(".pixel");
-    for(i = 0;i < pixel.length;i += 1){
-        pixel[i].addEventListener("click",function(e){
+    let pixel = document.querySelectorAll(".pixel");
+    for (i = 0; i < pixel.length; i += 1) {
+        pixel[i].addEventListener("click", function (e) {
             let seletor = document.querySelector(".selected")
-            if(e.target.classList.length > 1){
+            if (e.target.classList.length > 1) {
                 e.target.classList.remove(e.target.classList[1])
             }
             e.target.classList.add(seletor.classList[1]);
         })
+    }
+
+    let reset = document.querySelector("#clear-board")
+    reset.addEventListener("click", restart)
+    function restart() {
+        for (indice = 0; indice < pixel.length; indice += 1) {
+            pixel[indice].classList.add("white")
+        }
     }
 }
